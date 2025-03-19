6762cf2bacacf4d1ef3d974e0f5e4da43f7587e4
Engineer <ashalyapin@ya.ru>
2025-03-19 16:02:16 +0300

    add table of records in main

--- GET_Request.py
+++ GET_Request.py
@@ -1,6 +1,15 @@
 import requests
 
 url = 'http://185.18.54.154:8000/myapp/receive_data/'
+url2 = 'http://185.18.54.154:8000/myapp/clear_data/'
+
+def clear_table():
+    global url2
+    response = requests.get(url2)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
 response = requests.get(url)
 
 if response.status_code == 200:
